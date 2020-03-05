3be54e9b3bc6c5a98ce7933c823ff55b2a4e50c9
Egor <egoridze81@gmail.com>
2020-03-05 14:51:58 +0700

    sms

--- routes/sms.js
+++ routes/sms.js
@@ -2,18 +2,24 @@ const router = require('express').Router();
 const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 const smsRu = require('sms_ru');
 
+const fs = require('fs');
+
 const sms = new smsRu('7CEF047D-F5C4-41AF-966A-B12EA5B3106F');
 
 
 const Telefone = require('../models/Telefone');
 
-router.post('/send', ensureAuthenticated, (req, res) => {
-    sms.sms_send({
-        to: '79141410483',
+router.post('/send', ensureAuthenticated, async (req, res) => {
+    const telephones = await Telefone.find({});
+
+    telephones.forEach(item => {
+      sms.sms_send({
+        to: item.telefone,
         text: req.body.text
       }, function(e){
         res.send(JSON.stringify({status : 'ok'}));
       });
+    })
 })
 
 module.exports = router;
